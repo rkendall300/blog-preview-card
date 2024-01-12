99993b8068cbb6d2d53cb5c71c88967f1ee12cba
Ryan Kendall <ryekendallion@gmail.com>
2024-01-12 16:37:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import footerImage from './images/image-avatar.webp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="backdrop">
+      <div id="card">
+        <div id="fm-image-container">
+          <div id="fm-image"></div>
+        </div>
+        <div id="info">
+          <div id="tag">
+            <p id="tag-text">Learning</p>
+          </div>
+          <p id="publish-date">Published 21 Dec 2023</p>
+          <h1>HTML & CSS foundations</h1>
+          <p id="description">These languages are the backbone of every website, defining structure, content, and presentation.</p>
+          <div id="footer">
+            <img src={footerImage} alt="avatar" id="avatar"></img>
+            <h2 id="author">Greg Hooper</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
